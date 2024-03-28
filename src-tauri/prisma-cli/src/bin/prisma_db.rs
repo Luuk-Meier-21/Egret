7aeadf04b6858a631843b3ace9bbad376e13b3e2
Luuk Meier <luuk@pixelpillow.nl>
2024-03-28 16:21:13 +0100

    Prisma working!

--- src-tauri/prisma-cli/src/bin/prisma_db.rs
+++ src-tauri/prisma-cli/src/bin/prisma_db.rs
@@ -1,3 +1,6 @@
+use prisma_client_rust_cli;
+
 fn main() {
+    println!("Test");
     prisma_client_rust_cli::run();
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,33 +2,36 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[allow(warnings, unused)]
-mod prisma;
+mod db;
 
-// #[allow(warnings, unused)]
-// mod prisma;
+use db::*;
+use prisma_client_rust::tokio;
+use serde::Deserialize;
+use specta::{collect_types, Type};
+use std::sync::Arc;
+use tauri::State;
+use tauri_specta::ts;
 
-// use prisma_client_rust::{tokio, PrismaClient};
-// use specta::{collect_types, Type};
-// use tauri_specta::ts;
+type DbState<'a> = State<'a, Arc<PrismaClient>>;
 
-// #[tauri::command]
-// #[specta::specta]
-// fn get_users(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
+#[tauri::command]
+#[specta::specta]
+fn get_users(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
 
-// #[tokio::main]
-fn main() {
-    // let db = PrismaClient::_builder().build().await.unwrap();
+#[tokio::main]
+async fn main() {
+    let db = PrismaClient::_builder().build().await.unwrap();
 
-    // #[cfg(debug_assertions)]
-    // ts::export(collect_types![get_users], "../src/bindings.ts").unwrap();
+    #[cfg(debug_assertions)]
+    ts::export(collect_types![get_users], "../src/bindings.ts").unwrap();
 
-    // #[cfg(debug_assertions)]
-    // db._db_push().await.unwrap();
+    #[cfg(debug_assertions)]
+    db._db_push().await.unwrap();
 
-    // tauri::Builder::default()
-    //     .invoke_handler(tauri::generate_handler![get_users])
-    //     .run(tauri::generate_context!())
-    //     .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_users])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
