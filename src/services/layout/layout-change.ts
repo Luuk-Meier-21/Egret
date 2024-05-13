b0a4c7d5d1ee256c34826e97a316d46f431acf3e
Luuk Meier <luuk@pixelpillow.nl>
2024-05-13 14:14:01 +0200

    Layout change

--- src/services/layout/layout-change.ts
+++ src/services/layout/layout-change.ts
@@ -1,6 +1,25 @@
 import { DependencyList, EffectCallback, useEffect, useRef } from "react";
 import { objectIsEqual } from "../../utils/object";
 
+export function useStrictEffect<
+  DT extends DependencyList,
+  RT extends number | boolean | string,
+>(effect: EffectCallback, normalize: (deps: [...DT]) => RT, deps: [...DT]) {
+  const prefRef = useRef<RT>(normalize(deps));
+
+  useEffect(() => {
+    const current = normalize(deps);
+    let destructor: any = () => {};
+
+    if (current !== prefRef.current) {
+      destructor = effect();
+    }
+
+    prefRef.current = current;
+    return destructor;
+  }, deps);
+}
+
 export function useObservableEffect(
   effect: EffectCallback,
   deps?: DependencyList | undefined,
